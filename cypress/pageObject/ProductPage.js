f9ddec5fe21fae99d0f4f181d6698ecbd3b9adab
Jaxvor <98821932+Jaxvor@users.noreply.github.com>
2026-07-05 14:30:37 +0100

    Minor changes to remove verifications from pages to tests

--- cypress/pageObject/ProductPage.js
+++ cypress/pageObject/ProductPage.js
@@ -1,14 +1,7 @@
 class ProductPage {
     addToCart(){
-        //This will select a t-shirt from the homepage. The id's are set to specific items rather than generic id's which
-        //is potentially problematic should the items be removed from the site or stock runs out
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click()
-    }
-
-    removeFromCart() {
-        //The id is tied to the item in the cart so again this is potentially very flakey because the item may change name or 
-        //may be out of stock
-        cy.get('#remove-sauce-labs-bolt-t-shirt').click()
+        //This simply clicks the add to cart button on a product page.
+        cy.get('#add-to-cart').click()
     }
 }
 export default ProductPage
